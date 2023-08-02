fe1d53942d9707564fbeb378aec03b247a3231c6
Joe Dalton <65724078+J-Dalton@users.noreply.github.com>
2023-08-02 15:47:42 +0100

    Matching database connection info

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,5 +1,5 @@
 <?php 
 
-$pdo = new PDO('mysql:host=localhost; dbname=mdb_jd4986m; charset=utf8', 'user', 'password');
+$pdo = new PDO('mysql:host=localhost; dbname=mdb_jd4986m; charset=utf8', 'root', '123');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
