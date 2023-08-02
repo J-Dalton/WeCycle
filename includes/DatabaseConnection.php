328ca30e3ae3271cf80af1b4f021cac5f3c008e2
Joe Dalton <65724078+J-Dalton@users.noreply.github.com>
2023-08-02 15:34:51 +0100

    Modified database connection details

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,6 +1,5 @@
 <?php 
 
-//$pdo = new PDO('mysql:host=mysql.cms.gre.ac.uk; dbname=mdb_jd4986m; charset=utf8', 'jd4986m', 'jd4986m');
-$pdo = new PDO('mysql:host=localhost; dbname=mdb_jd4986m; charset=utf8', 'root', '123');
+$pdo = new PDO('mysql:host=localhost; dbname=mdb_jd4986m; charset=utf8', 'user', 'password');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
